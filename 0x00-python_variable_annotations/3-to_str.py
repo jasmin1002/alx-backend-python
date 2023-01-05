5de166ca036d29e805ca830bd6e530ae76a293af
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-05 17:33:40 +0100

    feat: Implement string transform for float number

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Basic Annotations"""
+
+
+def to_str(n: float) -> str:
+    """to_str function with PEP 484 type annotations
+    It forms the string representation of given input.
+
+    Args:
+        n: input number to transform string
+
+    Returns:
+        returns string representation of float number
+    """
+    return str(n)
