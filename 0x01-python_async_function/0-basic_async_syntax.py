119d3e11eded179c0063663f458acb6790edb10f
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-09 16:03:03 +0100

    feat: Demo async await syntax

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Basic asynchronous code"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Async wait_random delays program within of [0 - max_delay)
+    or [0 - max_delay] depending on rounding
+
+    Args:
+        max_delay: upper boundary
+
+    Returns:
+        returns amount of time in sec the program is paused.
+    """
+    rand = random.uniform(0, max_delay)
+    await asyncio.sleep(rand)
+    return rand
