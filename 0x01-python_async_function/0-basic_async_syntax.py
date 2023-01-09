24abb552106d31d9d7372b11a21e7d4ac2637014
Jasmin1002 <jasmineolofin@gmail.com>
2023-01-09 16:15:13 +0100

    feat: Mark the data with type annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay: int = 10) -> float:
     """
     Async wait_random delays program within of [0 - max_delay)
     or [0 - max_delay] depending on rounding
